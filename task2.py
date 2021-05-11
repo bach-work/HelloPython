b6ee0dabb354e72f2d7a55a545ade4522156a4fe
bach <bach.work@yandex.ru>
2021-05-11 21:27:38 +0300

    Добавил задачу с генератором

---
+++ task2.py
@@ -0,0 +1,11 @@
+# 2. Представлен список чисел. Необходимо вывести элементы исходного списка, значения
+# которых больше предыдущего элемента
+
+source = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]  # Исходный массив
+
+# Первый вариант через range
+result = [source[i + 1] for i in range(len(source) - 1) if source[i] < source[i + 1]]
+print(result)
+# Второй вариант через enumerate и срез без последнего элемента (ибо проверяем на i+1)
+result = [source[i + 1] for i, el in enumerate(source[:-1]) if el < source[i + 1]]
+print(result)

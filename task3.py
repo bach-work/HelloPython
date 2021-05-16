b169a4da71e1847adb3ae0f724c92a934099f357
bach <bach.work@yandex.ru>
2021-05-16 15:05:52 +0300

    Добавил 4ю задачу

--- task3.py
+++ task3.py
@@ -4,12 +4,12 @@
 # сотрудников.
 from file_generator import text_gen
 
-workers = ['Иванов','Петров','Сидоров','Попов','Козлов','Шац','Кузнецов','Чернов','Овсов','Питонов']
+workers = ['Иванов', 'Петров', 'Сидоров', 'Попов', 'Козлов', 'Шац', 'Кузнецов', 'Чернов', 'Овсов', 'Питонов']
 if not text_gen.gen_salary_file('f_task3.txt', workers):
     print('Ошибка формирования файла')
     exit(-1)
 
-with open('f_task3.txt', 'rt',  encoding='utf-8') as f:
+with open('f_task3.txt', 'rt', encoding='utf-8') as f:
     try:
         salary_list = []
         for name_line in f:  # для каждого сотрудника
@@ -18,6 +18,6 @@ with open('f_task3.txt', 'rt',  encoding='utf-8') as f:
             salary_list.append(salary)
             if salary > 20000.00:
                 print(f'{name} {salary}')
-        print(f'Средняя зп сотрудников {sum(salary_list)/len(salary_list):.2f}')
+        print(f'Средняя зп сотрудников {sum(salary_list) / len(salary_list):.2f}')
     except BaseException as e:
         print('Призошла ошибка при чтении файла', e)
